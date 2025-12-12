d611cafca28ed23ea6eadd191dbf9a70aa09b38c
barkoko <barkokomar@gmail.com>
2025-12-12 23:15:43 +0100

    ajout XMLUtils.cs pour transformation automatique à la sortie du jeux

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
 ﻿using var game = new CatRoyale.Scripts.CatRoyal();
 game.Run();
+CatRoyale.Scripts.XMLUtils.XslTransform("../../../data/xml/autoSave.xml", "../../../data/xslt/autoSave_to_html.xsl", "../../../data/html/autoSave.html"); 
